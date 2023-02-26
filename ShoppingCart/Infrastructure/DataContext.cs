1f647c17f3a150f8741b6f9f3e71c4e46772ca31
Hikimory <nikita.bikon@gmail.com>
2023-02-26 16:09:49 +0200

    Add DbContext and Models

---
+++ ShoppingCart/Infrastructure/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Models;
+
+namespace ShoppingCart.Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
